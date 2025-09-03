bda2f23513e24d1f5b85559843d5f2f8664079cc
prince005151 <princekumar005151@gmail.com>
2025-09-04 00:37:16 +0530

    writing some routes and aggregatin pipelines

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,4 +1,4 @@
-import { userRegister,userLogin, userLogout,refreshAccessToken} from "../controllers/user.controller";
+import { userRegister,userLogin, userLogout,refreshAccessToken, changePassword, getCurrentUser} from "../controllers/user.controller";
 import Router from "express-Router"
 import { verifyJWT } from "../middlewares/auth.middlewares";
 const router = Router();
@@ -7,5 +7,8 @@ router.route("/login").post(verifyJWT,userLogin)
 
 router.route('/logout').post(userLogout)
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/change_password").post(changePassword)
+router.route("/currentUser").post(getCurrentUser)
+router.route("/acountDetails").post("updateAccountDetails")
 
 export default {router}
